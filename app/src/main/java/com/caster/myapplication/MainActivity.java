4916cddf109bd07b44c726d30cbffb0c68a2ee93
mwolfson <mwolfson@gmail.com>
2016-08-19 12:10:22 -0700

    Initial Project commit
    Initial project with simple CollapsingToolbar implemented.

---
+++ app/src/main/java/com/caster/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.caster.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("Action Bar Title");
+
+    }
+}
